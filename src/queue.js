45d6cd3d982592fc1f6f1fb95832e48df8a025b5
lebedeananastasija <lebedeanastasija@yandex.ru>
2016-08-22 02:27:31 +0300

    with priority queue

--- src/queue.js
+++ src/queue.js
@@ -2,23 +2,38 @@ const MaxHeap = require('./max-heap.js');
 
 class PriorityQueue {
 	constructor(maxSize) {
-
+		if(maxSize === undefined) 
+			this.maxSize = 30;
+		else
+			this.maxSize = maxSize;
+		this.heap = new MaxHeap();
 	}
 
 	push(data, priority) {
-
+		if(this.maxSize > this.size()) {
+			this.heap.push(data, priority);
+		}
+		else 
+			throw "Queue is full";
 	}
 
 	shift() {
-
+		if(this.size() > 0) {
+			return this.heap.pop();
+		}
+		else
+			throw "Queue is empty";
 	}
 
 	size() {
-
+		return this.heap.size();
 	}
 
 	isEmpty() {
-		
+		if(this.size() == 0)
+			return true;
+		else
+			return false;
 	}
 }
 
